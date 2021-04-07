b5d2f686f1c41c52f7c0025c46550e9c74c8b34a
nguyendacchien <nguyendacchien02032002@gmail.com>
2021-04-07 14:13:54 +0700

    fsa

---
+++ toantu.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("chiều rộng");
+inputHeight = prompt("chiều cao");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("The area is: " + area);
